8358e54c843deabad03cfe792a38a4091a464053
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-06-01 09:22:16 -0400

    v_1 of Grader constructor, inputNewGrade method (wip)

--- src/com/company/Grader.java
+++ src/com/company/Grader.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Grader
 {
@@ -9,8 +10,17 @@ public class Grader
     private ArrayList<String> gradingCriteria;
     private int[] criteriaWieght;
     private int[] gradingScale;
+    private double[][] gradeHolder;
 
-    //constructor for the Grader Class, with criteria, weight, and scale as parameters
+    /**
+     * Constructs a grader object.
+     * @param criteriaInput String ArrayList of the criteria being graded in a class (e.g. Homework, Quizzes,
+     *                      Participation...).
+     * @param weightInput Int Array of how much weight is given to each criteria (e.g. 20%, 30%, 10%...) index of weight
+     *                    should correspond with the index of the criteria with that weight.
+     * @param scaleInput Int Array to specify a non-standard grading scale. Index 0: A+, 1: A, 2: A-... The ints
+     *                   represent the lowest in the grading range (e.g. [97, 93, 90,...]
+     */
     public Grader(ArrayList<String> criteriaInput, int[] weightInput, int[] scaleInput)
     {
         //Assigns private variable to the appropriate constructor parameters
@@ -19,5 +29,18 @@ public class Grader
         gradingCriteria = new ArrayList<>(criteriaInput);
         criteriaWieght = Arrays.copyOf(weightInput, weightInput.length);
         gradingScale = Arrays.copyOf(scaleInput, scaleInput.length);
+        gradeHolder = new double[gradingCriteria.size()][70];
+    }
+
+    public void inputNewGrade()
+    {
+        System.out.println("Please enter new grades for the following criteria");
+        Scanner in = new Scanner(System.in);
+        for(String element: gradingCriteria)
+        {
+            System.out.println(element + ":");
+            double newGrade = in.nextDouble();
+
+        }
     }
 }
