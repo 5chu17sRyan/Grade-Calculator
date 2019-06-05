d0377b915826e1018e92463ea8ab395469037701
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-06-05 12:58:40 -0400

    "Main" criteriaInput and weightInput code.

--- src/com/company/Grader.java
+++ src/com/company/Grader.java
@@ -36,12 +36,13 @@ public class Grader
     private int criteria19_GradeCounter = 0;
     private int criteria20_GradeCounter = 0;
     private int criteriaLength;
+    private final int MAX_GRADING_CRITERIA = 20;
 
     /**
      * Constructs a grader object.
      * @param criteriaInput String Array of the criteria being graded in a class (e.g. Homework, Quizzes,
      *                      Participation...).
-     * @param criteriaTracker Keeps track of how many elements are stored in the criteriaInput array. The value
+     * @param criteriaSize Keeps track of how many elements are stored in the criteriaInput array. The value
      *                        of criteriaTracker corresponds to the last filled index in the array, under the assumption
      *                        that there are no gaps in the array up till that point.
      * @param weightInput Int Array of how much weight is given to each criteria (e.g. 20%, 30%, 10%...) index of weight
@@ -49,18 +50,18 @@ public class Grader
      * @param scaleInput Int Array to specify a non-standard grading scale. Index 0: A+, 1: A, 2: A-... The ints
      *                   represent the lowest in the grading range (e.g. [97, 93, 90,...])
      */
-    public Grader(String[] criteriaInput, int criteriaTracker, int[] weightInput, int[] scaleInput)
+    public Grader(String[] criteriaInput, int criteriaSize, int[] weightInput, int[] scaleInput)
     {
         //Assigns private variable to the appropriate constructor parameters
         //Makes copies of the array parameters instead of using references so the private variables cannot be changed
         //outside the class methods
-        gradingCriteria = Arrays.copyOf(criteriaInput, criteriaTracker);
+        gradingCriteria = Arrays.copyOf(criteriaInput, criteriaSize);
         criteriaWieght = Arrays.copyOf(weightInput, weightInput.length);
         gradingScale = Arrays.copyOf(scaleInput, scaleInput.length);
-        criteriaLength = criteriaTracker;
+        criteriaLength = criteriaSize;
         //Twenty columns for a maximum of 20 different grading criteria. 70 rows for a maximum of 70 different criteria
         //grades
-        gradeHolder = new double[20][70];
+        gradeHolder = new double[MAX_GRADING_CRITERIA][70];
     }
 
     /**
@@ -74,7 +75,7 @@ public class Grader
         //Asks user to input new grades
         System.out.println("Please enter new grades for the following criteria.\n");
         //Creates a scanner "in" to track their input
-        Scanner in = new Scanner(System.in);
+        Scanner GradeIn = new Scanner(System.in);
         //Loop that executes for each existing criteria
         for(int i = 0; i <= criteriaLength; i++)
         {
@@ -82,10 +83,10 @@ public class Grader
             System.out.println(gradingCriteria[i] + ": ");
             //Loop that executes while there are still new grades for a criteria
             //Allows user to input multiple grades at the same time (e.g. Homework: "97 84 88.5")
-            while(in.hasNextDouble())
+            while(GradeIn.hasNextDouble())
             {
                 //Stores the next grade input in the variable newGrade
-                double newGrade = in.nextDouble();
+                double newGrade = GradeIn.nextDouble();
                 /*
                 Is there a way for me to make the criteriaGradeCounters change within the loop or do I have to use a
                 very long switch statement. Is there something that works like criteriaGradeCounter'i' that only
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //Asks user to input their grading criteria
+        System.out.println("What are you being graded on?\n" +
+                "(Enter all criteria names separated by a space before hitting return and enter them as you wish to " +
+                "see them in the future)\n");
+        //Creates scanner to read input
+        Scanner in = new Scanner(System.in);
+        //20 is the Maximum number of grading criteria allowed by this program
+        final int MAX_GRADING_CRITERIA = 20;
+        //Creates a string array to hold the names of the criteria
+        String[] criteriaInput = new String[MAX_GRADING_CRITERIA];
+        //Variable to track the current size of the criteriaInput array set to 0
+        int criteriaSize = 0;
+        //While there are still criteria names from the user execute the following code
+        while(in.hasNext())
+        {
+            //If there are more input than allowed
+            if(criteriaSize++ > MAX_GRADING_CRITERIA)
+            {
+                //Tells the user that they cannot have more than 20 criteria
+                System.out.println("20 is the maximum number of grading criteria allowed. Consider combining" +
+                        "categories with the same grading weight. For example, if both homework and participation" +
+                        "count for 20% of the entire grade combine the two into a category " +
+                        "\"Homework/Participation\"\n");
+                //Exits while loop so no more than twenty names are input into the array.
+                break;
+            }
+            //Stores next criteria name in next available array element
+            criteriaInput[criteriaSize] =  in.next();
+            //Keeps track of array size by incrementing by one
+            criteriaSize++;
+        }
+
+        //Asks the user for the weight of each criteria
+        System.out.println("How are these criteria weighted?");
+        //Creates array the size of criteriaInput to hold the weights of each criteria
+        int[] weightInput = new int[criteriaSize];
+        //Executes the following block for each criteria
+        for(int i = 0; i <= criteriaSize; i++)
+        {
+            //Prints out name of criteria followed by a colon
+            System.out.println(criteriaInput[i] + ": ");
+            //Fills in the array with the corresponding criteria weight
+            weightInput[i] = in.nextInt();
+        }
+
 
     }
 }
